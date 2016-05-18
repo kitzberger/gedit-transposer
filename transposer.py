53cfbe369c0ae9361a0d47564e7770bb30a7a667
Philipp Kitzberger <philipp@kitze.net>
2016-05-19 01:11:56 +0200

    Compatibility to gedit >= 3.12

--- transposer.py
+++ transposer.py
@@ -1,27 +1,44 @@
 from gettext import gettext as _
-from gi.repository import GObject, Gtk, Gedit
+from gi.repository import GObject, Gtk, Gio, Gedit
 import re
 import pprint
 
-# Menu item example, insert a new item in the Tools menu
-ui_str = """<ui>
-    <menubar name="MenuBar">
-        <menu name="ToolsMenu" action="Tools">
-            <placeholder name="Transpose">
-                <menuitem name="TransposeUp" action="TransposeUp"/>
-                <menuitem name="TransposeDown" action="TransposeDown"/>
-            </placeholder>
-        </menu>
-    </menubar>
-    <toolbar name="ToolBar">
-        <separator />
-        <toolitem name="TransposeToolItemUp" action="TransposeUp" />
-        <toolitem name="TransposeToolItemDown" action="TransposeDown" />
-    </toolbar>
-</ui>
-"""
-class TransposerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
-    __gtype_name__ = "TransposerWindowActivatable"
+ACTIONS = {
+    'transposeUp': {
+        'label': _("Transpose chords up"),
+        'key':   ['<Ctrl><Alt>u']
+    },
+    'transposeDown': {
+        'label': _("Transpose chords down"),
+        'key':   ['<Ctrl><Alt>d']
+    }
+}
+
+class GeditTransposerApp(GObject.Object, Gedit.AppActivatable):
+    __gtype_name__ = "GeditTransposerApp"
+    app = GObject.property(type=Gedit.App)
+
+    def __init__(self):
+        GObject.Object.__init__(self)
+
+    def do_activate(self):
+        # Single menu items & shortcuts
+        self.menu_ext = self.extend_menu("tools-section")
+        for action, config in ACTIONS.items():
+            item = Gio.MenuItem.new(config['label'], "win.%s" % action)
+            self.menu_ext.prepend_menu_item(item)
+            self.app.set_accels_for_action("win.%s" % action, config['key'])
+
+    def do_deactivate(self):
+        for action, config in ACTIONS.items():
+            self.app.set_accels_for_action("win.%s" % action, [])
+        del self.menu_ext
+
+    def do_update_state(self):
+        pass
+
+class GeditTransposerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "GeditTransposerWindow"
 
     window = GObject.property(type=Gedit.Window)
 
@@ -29,50 +46,21 @@ class TransposerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        # Insert menu items
-        self._insert_menu()
+        try:
+            action = Gio.SimpleAction(name="transposeUp")
+            action.connect('activate', lambda e,f: self.on_transpose(1))
+            self.window.add_action(action)
+            action = Gio.SimpleAction(name="transposeDown")
+            action.connect('activate', lambda e,f: self.on_transpose(-1))
+            self.window.add_action(action)
+        except Exception as msg:
+            import traceback
+            print("Error initializing \"Transposer\" plugin")
+            print(traceback.print_exc())
 
-    def do_deactivate(self):
-        # Remove any installed menu items
-        self._remove_menu()
-
-        self._action_group = None
-
-    def _insert_menu(self):
-        # Get the Gtk.UIManager
-        manager = self.window.get_ui_manager()
-
-        # Create a new action group
-        self._action_group = Gtk.ActionGroup("TransposerPluginActions")
-        self._action_group.add_actions([
-            ("TransposeUp", None, _("Transpose chords by +1"), None, _("Transpose chords by +1"), lambda e: self.on_transpose(None, 1)),
-            ("TransposeDown", None, _("Transpose chords by -1"), None, _("Transpose chords by -1"), lambda e: self.on_transpose(None, -1))
-        ])
-
-        # Insert the action group
-        manager.insert_action_group(self._action_group, -1)
-
-        # Merge the UI
-        self._ui_id = manager.add_ui_from_string(ui_str)
-
-    def _remove_menu(self):
-        # Get the Gtk.UIManager
-        manager = self.window.get_ui_manager()
-
-        # Remove the ui
-        manager.remove_ui(self._ui_id)
-
-        # Remove the action group
-        manager.remove_action_group(self._action_group)
-
-        # Make sure the manager updates
-        manager.ensure_update()
-
-    def do_update_state(self):
-        self._action_group.set_sensitive(self.window.get_active_document() != None)
 
     # Menu activate handlers
-    def on_transpose(self, action, transposeBy):
+    def on_transpose(self, transposeBy):
         doc = self.window.get_active_document()
 
         if not doc:
